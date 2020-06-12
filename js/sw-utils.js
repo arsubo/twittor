ac7e4df23d71ba0cc8276d0ba0ed9ccb5f7b8163
Arnoldo Rafael Suarez Bonilla <asuarez@cafesoluble.com>
2020-06-11 19:17:23 -0600

    Modificación URL3

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -5,7 +5,7 @@ function actualizaCacheDinamico(dynamicCache, req, res) {
     if (res.ok) {
        return  caches.open(dynamicCache).then(cache => {
             cache.put(req, res.clone());
-            res.clone()
+            return res.clone()
         })
     } else {
         return res;
